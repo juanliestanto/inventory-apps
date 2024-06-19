d83d7d9eea8eafa6873e1a55baefd66ea43f6520
JuanLiestanto <@liestantojuandwiky@gmail.com>
2024-06-19 20:55:43 +0700

    [FEATURE]

---
+++ inventory-apps/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inventory_apps.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
